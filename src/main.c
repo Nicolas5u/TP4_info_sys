7f7c74c3cce508ce95e87ff3c207d5cf8ac7bf7e
Nicolas <nicolasdurand34080@gmail.com>
2025-10-08 17:03:54 +0200

    juste avant de réalisation en boucles

--- src/main.c
+++ src/main.c
@@ -6,25 +6,35 @@
 
 int main(){
   char rep[255];
-  printf ("interpréteur-de-commande-de-Nicolas$ ");
+  char * pos_pb;
+  char * argv[];
   
   /*
   char * buffer = (char*) malloc(sizeof(rep));
   */
   
-  fgets(rep, sizeof(rep), stdin);
-  // on enlève l'élément \0 qui était l'avant dernier élément du char
-  char * pos_pb = strchr(rep,'\n');
-  * pos_pb = '\0';
+  while (strcmp(rep, "exit") != 0){
+  
+      printf ("interpréteur-de-commande-de-Nicolas$ ");
+      
+      // on récupère ce qui a été envoyé
+      fgets(rep, sizeof(rep), stdin);
+      
+      // on enlève l'élément \0 qui était l'avant dernier élément du char
+      * pos_pb = strchr(rep,'\n');
+      * pos_pb = '\0';
+      
+      // gestion des commandes simples
+      * argv[] = {rep,NULL};
+      execvp(argv[0],argv);
+      perror("execvp");
+  }
+  
   
   if (strcmp(rep, "exit") == 0){
       printf ("tu as tapé exit pour sortir de l'interpréteur de commande de Nicolas\n ");
       return 1;
   }
-  
-  char * argv[] = {rep,NULL};
-  execvp(argv[0],argv);
-  perror("execvp");
   
   return 1;
 }
